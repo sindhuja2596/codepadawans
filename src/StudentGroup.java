5f96fcc376dfcf683fa572efeedb774b60f3c0b5
Sindhuja Anumoju <kucsegirls@gmail.com>
2017-09-16 23:54:00 +0530

    Implemented getStudents()

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -25,8 +25,7 @@ public class StudentGroup implements StudentArrayOperation {
 
 	@Override
 	public Student[] getStudents() {
-		// Add your implementation here
-		return null;
+		return students;
 	}
 
 	@Override
